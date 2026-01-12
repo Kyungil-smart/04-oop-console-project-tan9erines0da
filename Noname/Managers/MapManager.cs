11ff590026ccef8ebb8ad3bac0ae5e113bcfe2ce
강세환(플밍4기) <ghks5658@gmail.com>
2026-01-12 18:08:11 +0900

    보이는 맵 기능 부분 구현
    보이는 맵 배열 구현
    보이는 맵 배열 수정을 위한 타일 매서드 구현

--- Noname/Managers/MapManager.cs
+++ Noname/Managers/MapManager.cs
@@ -7,14 +7,21 @@ public class MapManager
     // 맵 상태를 변경하는 것과 관련된 필드와 메서드
 
     public Mapdata1 _mapinfo = new Mapdata1();
-    public int[,] MazeTileGrid = new int[5, 5];
-
+    public Dictionary<int[,], MazeTile> _mazepoint= new Dictionary<int[,], MazeTile>();
+    // 좌표를 딕셔너리의 키로 좌표에 있는 미로 타일의 정보 관리
+    
 
     public int _playerY;
     public int _playerX;
 
     public int _goalX;
     public int _goalY;
+
+    public MapManager()
+    {
+        //초기 맵 생성, 무작위 셔플
+
+    }
 
     
 }
--- Noname/MazeTile.cs
+++ Noname/MazeTile.cs
@@ -5,15 +5,71 @@ using System.Text;
 public class MazeTile
 {
     public int _tilenumber;//타일 넘버
-    public int _gridX;
-    public int _gridY;//좌표
-    //맵 정보는 타일 넘버에 종속되는거니까 굳이?
+    public int[] _tileinfo = new int[48];  //출력에 필요한 정보
 
-    public MazeTile(int order, int gridX, int gridY)
+
+    public MazeTile(int order, int[,] map)
     {
         _tilenumber = order;
-        _gridX = gridX;
-        _gridY = gridY;
+        // 생성시 타일 넘버 부여
+        Readinfo(order, map);
+    }
+
+    public void Readinfo(int order, int[,] map)  // 순서에 따른 맵 출력 형태 배열 할당
+    {
+
+        int x = IgniteX(order);  // 타일 번호에 따른 출력 시작점
+        int y = IgniteY(order);  //
+        
+        _tileinfo[0] = map[y, x];
+        for (int i = 1; i <= 5; i++)
+        {
+            _tileinfo[i] = map[y+1, x-3 + i];
+        }
+        for (int i = 6; i <= 41; i++)
+        {
+            for (int j = y+2; j <= y+5; j++)
+            {
+                for (int k = x-4; k <= x+4; k++)
+                    _tileinfo[i] = map[j, k];
+            }
+        }
+        for (int i = 42; i <= 46; i++)
+        {
+            _tileinfo[i] = map[y+6, x-3 + i];
+        }
+        _tileinfo[47] = map[y+7, x];
 
     }
+
+    public int IgniteX(int x)  //x좌표 점화식
+    {
+        if (x < 4)
+            return 10 * x + 4;
+        else if (x < 8)
+            return 10 * (x - 4) + 9;
+        else if (x < 13)
+            return 10 * (x - 8) + 4;
+        else if (x < 17)
+            return 10 * (x - 13) + 9;
+        else
+            return 10 * (x - 16) + 4;
+
+    }
+
+    public int IgniteY(int y)  //y좌표 점화식
+    {
+        if (y < 4)
+            return 1;
+        else if (y < 8)
+            return 6;
+        else if (y < 13)
+            return 11;
+        else if (y < 17)
+            return 16;
+        else
+            return 21;
+    }
 }
+
+
